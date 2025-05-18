e04c56ff056177580ec21cc103eddbc93db4e7db
luisbaeza11 <54044375+luisbaeza11@users.noreply.github.com>
2025-05-17 20:59:13 -0600

    Update script.js

--- script.js
+++ script.js
@@ -7,6 +7,13 @@ const alarmas = {
         "urgencia": "Alta"
      },
 
+    "A002": {
+        "descripcion": "Hongo de emergmcia activp",
+        "causa": "Se oprimio el Hongo del panel operador, falla posible en el contacto N.C.",
+        "solucion": "1. Restablesca el Hongo de emergncia (rote y tire del hongo hacia arriba, oprima reset. 2. Verifique el cableado. 3. Reinicie.",
+        "urgencia": "Alta"
+     },
+
     "B205": {
         "descripcion": "Fuga de refrigerante",
         "causa": "Tubería obstruida o conexión floja",
